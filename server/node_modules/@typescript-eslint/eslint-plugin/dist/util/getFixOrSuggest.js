714159a8e6f344202bb50f71b5fc97c5a48f065a
Phil Sturgeon <67381+philsturgeon@users.noreply.github.com>
2025-01-07 12:36:12 +0000

    Who is JUnit sir

--- server/node_modules/@typescript-eslint/eslint-plugin/dist/util/getFixOrSuggest.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getFixOrSuggest = getFixOrSuggest;
-function getFixOrSuggest({ useFix, suggestion, }) {
-    return useFix ? { fix: suggestion.fix } : { suggest: [suggestion] };
-}
-//# sourceMappingURL=getFixOrSuggest.js.map
